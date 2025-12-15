db7228f4fc48dc5f28923363017ee524fbf486be
irlplay <irlplays228@gmail.com>
2025-12-15 19:57:45 +0700

    Update SetbackTeleportUtil.java

--- common/src/main/java/ac/grim/grimac/manager/SetbackTeleportUtil.java
+++ common/src/main/java/ac/grim/grimac/manager/SetbackTeleportUtil.java
@@ -141,10 +141,8 @@ public class SetbackTeleportUtil extends Check implements PostPredictionCheck {
             if (player.hasGravity)
                 vector.add(0.0D, -player.gravity / 4.0D, 0.0D);
         } else if (player.isGliding) {
-            PredictionEngineElytra.getElytraMovement(player, vector, ReachUtils.getLook(player, player.yaw, player.pitch)).multiply(player.stuckSpeedMultiplier).multiply(0.99F, 0.98F, 0.99F);
-            vector.setY(vector.getY() - 0.05); // Make the player fall a bit
-            vector.setX(originalX);
-            vector.setZ(originalZ);
+            PredictionEngineElytra.getElytraMovement(player, vector, ReachUtils.getLook(player, player.yaw, player.pitch)).multiply(player.stuckSpeedMultiplier);
+            vector.setY(vector.getY() * 0.98F - 0.05); // Make the player fall a bit
         } else { // Gliding doesn't have friction, we handle it differently
             PredictionEngineNormal.staticVectorEndOfTick(player, vector); // Lava and normal movement
         }
